1a5005f5965321dadc62ae8c4bdfd3b11671e4c7
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-01-29 19:46:03 +0100

    Add character and camera
    1st person navigation possible but render distance and light not set

---
+++ OriginIsUnreachable/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : MonoBehaviour {
+
+    public float vitesse;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (!isLocalPlayer)
+        //    return;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1.5f * vitesse;
+        var z = Input.GetAxis("Vertical") *  Time.deltaTime * vitesse;
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0,x,0);
+	}
+}
