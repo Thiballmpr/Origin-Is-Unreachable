c6b8fdfbae08e998ce080135022f666fa0b428d8
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-01-30 01:22:41 +0100

    Try main menu

---
+++ Main menu/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public float vitesse;
+
+    private void Start()
+    {
+        //Network.Instantiate(pl, new Vector3(-98, 130, -4300));
+    }
+
+    void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1.5f * vitesse;
+        var z = Input.GetAxis("Vertical") *  Time.deltaTime * vitesse;
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0,x,0);
+	}
+}
