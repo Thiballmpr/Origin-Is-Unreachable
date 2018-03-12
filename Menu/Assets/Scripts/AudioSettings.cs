6f5f2b00fb782f61025cd8c87a60cf2a79ce4da1
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-03-12 22:41:45 +0100

    AdvancedMenuSliders

---
+++ Menu/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+
+    public AudioMixer audioMasterMixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMasterMixer.SetFloat("volume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMasterMixer.SetFloat("MusicVol", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        audioMasterMixer.SetFloat("EffectsVol", volume);
+    }
+
+
+}
---
+++ Menu/Assets/Scripts/Randomize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Randomize : MonoBehaviour {
+
+	public int RandomizeEnemy(int nbPlayer)
+    {
+        System.Random nb = new System.Random();
+        int player = nb.Next(nbPlayer);
+        return player;
+    }
+}
