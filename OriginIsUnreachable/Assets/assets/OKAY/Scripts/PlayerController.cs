11270302b4cb57ddf47f0ecffa7c1721f2933634
Thomasvtn <Thomasvtn@users.noreply.github.com>
2018-03-12 22:16:59 +0100

    ttt

---
+++ OriginIsUnreachable/Assets/assets/OKAY/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float TranslateSpeed;
+    public float RotationSpeed;
+
+	void Update()
+	{
+        float x = Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * TranslateSpeed * Time.deltaTime;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
